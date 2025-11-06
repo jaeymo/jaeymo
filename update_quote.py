edfb06d664c44aba280c7f02bd60431adbf510e3
Jaeymo <jaey.cyber@gmail.com>
2025-11-05 21:06:40 -0600

    feat: more-quotes

--- update_quote.py
+++ update_quote.py
@@ -4,6 +4,15 @@ import re
 quotes = [
     '"Greatness demands sacrifice." — *The Rage of Dragons* by Evan Winters',
     '"The days without difficulty are the days you do not improve." — *The Rage of Dragons* by Evan Winters',
+    """ "It's never acceptable to be less than you are." — *The Rage of Dragons* by Evan Winters""",
+    """ "To defend against failure, every day must be hard. Every day must strengthen you. For it's in the crucible of hard days that potential becomes power." — *The Rage of Dragons* by Evan Winters """,
+    """ "Alliances made from convenience only ever weaken a cause." — *The Light of All That Falls* by James Islington""",
+    """ "You should never judge the sides of an argument simply by who is doing the arguing." — *The Light of All That Falls* by James Islington""",
+    """ "Truth can be a burden, but secrets are poison." — *The Light of All That Falls* by James Islington""",
+    """ "The muscle of willpower is invisible, but it's there." — *My Friend's Dad*""",
+    """ "The measure of a man is what he does when he has power." — *Red Rising* by Pierce Brown""",
+    """ "But of all the things in all the worlds, words are power." — *Red Rising* by Pierce Brown""",
+    ' "Our lives mean so much more than the frail bodies that carry them." — *Golden Son* by Pierce Brown ',
 ]
 
 quote = random.choice(quotes)
